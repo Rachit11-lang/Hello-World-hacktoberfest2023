19de1529e88409db6cb48cf09491c113b5ae2a13
Rachit11-lang <148413315+Rachit11-lang@users.noreply.github.com>
2023-10-20 23:33:26 +0545

    Create decimal.py

---
+++ decimal.py
@@ -0,0 +1,10 @@
+# Solve the equation: 3.5x + 2.2 = 10.8
+
+# Import the math library
+import math
+
+# Define the equation
+x = (10.8 - 2.2) / 3.5
+
+# Print the solution
+print("The solution for x is:", x)
